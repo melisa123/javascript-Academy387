1ece63c3543ef1a1eb709ab9fb4ac5d04ae95a37
melisa123 <melisa.babajic@gmail.com>
2016-04-02 00:24:35 +0200

    Revert "Pocetni commit"
    This reverts commit affcafdd2197b866eb832642da30f4cd264b438e.

--- Counting.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-var fileBuffer = fs.readFileSync(process.argv[2]);
-//console.log(fileBuffer);
-var fileText = fileBuffer.toString();
-//console.log(fileText);
-var array = fileText.split("\n");
-//console.log(array);
-console.log(array.length - 1);
